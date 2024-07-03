911f259a901271a1ce9c82126054fa43ee99a1a0
OmarCodes2 <omarcodes2@gmail.com>
2024-07-03 08:23:52 -0400

    update basepath and assetprefix

--- my-app/next.config.mjs
+++ my-app/next.config.mjs
@@ -1,13 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: 'export',
-    images: {
-      unoptimized: true,
-    },
-    basePath: process.env.NODE_ENV === 'production' ? '/OmarsPortfolio' : '',
-    assetPrefix: process.env.NODE_ENV === 'production' ? '/OmarsPortfolio/' : '',
-    trailingSlash: true,
-  };
-  
-  export default nextConfig;
-  
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
+  basePath: '',
+  assetPrefix: '',
+  trailingSlash: true,
+};
+
+export default nextConfig;
